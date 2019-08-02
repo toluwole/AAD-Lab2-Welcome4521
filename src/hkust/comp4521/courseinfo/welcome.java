8442cf3cb770a4e189344d3cd41ff9bc02f3e84d
Tope Oluwole <toluwole@aol.com>
2019-08-01 20:42:22 -0700

    Add base project files.

---
+++ src/hkust/comp4521/courseinfo/welcome.java
@@ -0,0 +1,14 @@
+package hkust.comp4521.courseinfo;
+
+import hkust.comp4521.courseinfo.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class welcome extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome);
+    }
+}
